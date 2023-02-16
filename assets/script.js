c23242255df52edf45a6ca2e702f445b6e017924
Richard Murray <114324221+RMurray22@users.noreply.github.com>
2023-02-15 16:48:59 -0800

    updated js code to final draft

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,66 @@
 function GetInfo() {
+  var newName = document.getElementById("cityInput");
+  var cityName = document.getElementById("cityName");
+  cityName.innerHTML = "--" + newName.value + "--";
 
-    var newName = document.getElementById("cityInput");
-    var cityName = document.getElementById("cityName");
-    cityName.innerHTML = "--" + newName.value + "--";
+  fetch(
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+      newName.value +
+      "&appid=debedf3ec770a6134bafe0d07ed54e45"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      //Getting the min and max values for each day
+      for (i = 0; i < 5; i++) {
+        document.getElementById("day" + (i + 1) + "Min").innerHTML =
+          "Min: " +
+          Number(data.list[i].main.temp_min - 273.15).toFixed(1) +
+          "°";
+      }
 
-    fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + newName.value + '&appid=debedf3ec770a6134bafe0d07ed54e45')
-        .then(response => response.json())
-        .then(data => {
+      for (i = 0; i < 5; i++) {
+        document.getElementById("day" + (i + 1) + "Max").innerHTML =
+          "Max: " +
+          Number(data.list[i].main.temp_max - 273.15).toFixed(2) +
+          "°";
+      }
+
+      //Getting Weather Icons
+      for (i = 0; i < 5; i++) {
+        document.getElementById("img" + (i + 1)).src =
+          "http://openweathermap.org/img/wn/" +
+          data.list[i].weather[0].icon +
+          ".png";
+      }
+
+      console.log(data);
+    });
+}
+
+function DefaultScreen() {
+  document.getElementById("cityInput").defaultValue = "London";
+  GetInfo();
+}
+
+var d = new Date();
+var weekday = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+function CheckDay(day) {
+  if (day + d.getDay() > 6) {
+    return day + d.getDay() - 7;
+  } else {
+    return day + d.getDay();
+  }
+}
+
+for (i = 0; i < 5; i++) {
+  document.getElementById("day" + (i + 1)).innerHTML = weekday[CheckDay(i)];
+}
